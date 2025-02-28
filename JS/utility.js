94868d88b195d792eb24ab234483417870d7eb83
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-02-28 22:29:15 +0600

    added navbar and functionality of theme changer button

---
+++ JS/utility.js
@@ -0,0 +1,14 @@
+let count = 0
+
+document.getElementById("theme-changer-btn")
+    .addEventListener("click", function () {
+        const colorList = ["#ACDDDE", "#CAF1DE", "#E1F8DC", "#FEF8DD", "#FFE7C7", "#F7D8BA"];
+        // let randomIndex = Math.round(Math.random()*(5-0));
+        // console.log(randomIndex)
+        let randomIndex = count%6;
+        document.body.style.backgroundColor = colorList[randomIndex];
+        count += 1;
+        
+
+
+    })
